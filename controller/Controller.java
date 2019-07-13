13b1d4b53c656046e314eb74120e463e1412506f
Antony.Glim <Antony.Glim@gmail.com>
2019-07-13 16:38:48 +0300

    3.

--- controller/Controller.java
+++ controller/Controller.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task36.task3608.controller;
 
 import com.javarush.task.task36.task3608.model.Model;
+import com.javarush.task.task36.task3608.view.UsersView;
 
 /**
  * Этот класс будет получать запрос от клиента,
@@ -10,6 +11,7 @@ import com.javarush.task.task36.task3608.model.Model;
 public class Controller {
 
     private Model model;
+    private UsersView usersView;
 
     public void setModel(Model model) {
         this.model = model;
@@ -21,5 +23,10 @@ public class Controller {
      */
     public void onShowAllUsers(){
         model.loadUsers();
+        usersView.refresh(model.getModelData());
+    }
+
+    public void setUsersView(UsersView usersView) {
+        this.usersView = usersView;
     }
 }
