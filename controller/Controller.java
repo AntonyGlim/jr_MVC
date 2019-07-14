cda0f682e013a68e505c7c542af7393dfc975fab
Antony.Glim <Antony.Glim@gmail.com>
2019-07-14 12:33:49 +0300

    6.

--- controller/Controller.java
+++ controller/Controller.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task36.task3608.controller;
 
 import com.javarush.task.task36.task3608.model.Model;
+import com.javarush.task.task36.task3608.view.EditUserView;
 import com.javarush.task.task36.task3608.view.UsersView;
 
 /**
@@ -12,6 +13,7 @@ public class Controller {
 
     private Model model;
     private UsersView usersView;
+    private EditUserView editUserView;
 
     public void setModel(Model model) {
         this.model = model;
@@ -33,5 +35,13 @@ public class Controller {
     public void onShowAllDeletedUsers() {
         model.loadDeletedUsers();
         usersView.refresh(model.getModelData());
+    }
+
+    public EditUserView getEditUserView() {
+        return editUserView;
+    }
+
+    public void setEditUserView(EditUserView editUserView) {
+        this.editUserView = editUserView;
     }
 }
--- model/MainModel.java
+++ model/MainModel.java
@@ -1,15 +1,12 @@
 package com.javarush.task.task36.task3608.model;
 
-import com.javarush.task.task36.task3608.bean.User;
 import com.javarush.task.task36.task3608.model.service.UserService;
 import com.javarush.task.task36.task3608.model.service.UserServiceImpl;
-
-import java.util.List;
 
 public class MainModel implements Model {
 
     private UserService userService = new UserServiceImpl();
-    private ModelData modelData =new ModelData();
+    private ModelData modelData = new ModelData();
 
     @Override
     public ModelData getModelData() {
@@ -18,10 +15,12 @@ public class MainModel implements Model {
 
     @Override
     public void loadUsers() {
+        modelData.setDisplayDeletedUserList(false);
         modelData.setUsers(userService.getUsersBetweenLevels(1, 100));
     }
 
     public void loadDeletedUsers() {
+        modelData.setDisplayDeletedUserList(true);
         modelData.setUsers(userService.getAllDeletedUsers());
     }
 }
